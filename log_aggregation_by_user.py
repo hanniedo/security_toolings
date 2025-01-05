30fba0cceb697d97287cf417124bc198ee207a5b
hanniedo <81393876+hanniedo@users.noreply.github.com>
2025-01-05 13:11:27 -0800

    Update log_aggregation_by_user.py

--- log_aggregation_by_user.py
+++ log_aggregation_by_user.py
@@ -23,37 +23,65 @@ Output:
 (['192.168.1.1'], ['user1'])
 
 '''
+from datetime import datetime 
 from collections import defaultdict
-from datetime import datetime
 
-def suspicious_login(logs:list):
+def suspicious_login_detection(logs):
 
-    suspicious_ips = defaultdict()
-    suspicious_users = defaultdict(list)
+    #A list of suspicious IP addresses with excessive failed attempts.
+    suspicious_ips = set()
+    #A list of users with suspicious multiple-IP logins.
+    suspicious_users = set()
+    #Initial a dict to store timestamp of the failed login attempts by ip
+    failed_attempts_by_ip = defaultdict(list)
+    #Initiate a dict to store ips of the success login attempts by username
+    success_attempts_by_user = defaultdict(list)
 
+    def log_parser(log):
+        timestamp, username, ip, status = log.split(',')
+        normalized_timestamp = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+        return {'timestamp': normalized_timestamp, 
+                'username': username,
+                'ip': ip,
+                'status': status}
     for log in logs:
-        timestamp, user_name, ip, status = log.split()
+        parsed_log = log_parser(log)
+        timestamp = parsed_log.get('timestamp', '')
+        username = parsed_log.get('username', '')
+        ip = parsed_log.get('ip')
+        status = parsed_log.get('status')
 
-        #1st scenario
+        #1st scenario: There are more than 3 failed login attempts from the same IP address within 10 minutes.
         if status == 'FAILED':
-            suspicious_ips[ip].append(datetime.strptime(timestamp, '%y-%m-%dT%H:%M:%S:'))
-            flagged_ips = set()
-            for ip, timestamps in suspicious_ips.items():
-                if len(sorted(timestamps)) >= 3:
-                    if (timestamps[:-1] - timestamps[0]).total_seconds() >= 600:
-                        flagged_ips.add(ip)
-        #2nd scenario
+            failed_attempts_by_ip[ip].append(timestamp)
+            #Exclude timestamps that are outside of the 10 min window.
+            failed_attempts_by_ip[ip] = [t for t in failed_attempts_by_ip[ip] if (timestamp-t).total_seconds() <= 600]
+            if len(failed_attempts_by_ip[ip]) >= 3:
+                 suspicious_ips.add(ip)
+
+        #2nd scenario: The same user logs in from two different IP addresses within 5 minutes.
         elif status == 'SUCCESS':
-            flagged_users = set()
-            suspicious_users[user_name].append((timestamp,ip))
-            #Remove IPs older than 5 mins
-            suspicious_users[user_name] = [(t,ip) for timestamp, ip in suspicious_users[user_name] if (timestamp - t).total_seconds() <= 300]
-            for username, event in suspicious_users.items():
-                if len(set(ip for _ in suspicious_users[user_name])) > 1:
-                    suspicious_users.add(user_name)
+            success_attempts_by_user[username].append((timestamp, ip))
+            #Exclude timestamps that are outside of the 5 min window 
+            success_attempts_by_user[username] = [(t, ip) for t,ip in success_attempts_by_user[username] if (timestamp -t).total_seconds() <= 300]
+            for username, event in success_attempts_by_user.items():
+                if len(set(ip for ip in event)) > 1 :
+                   suspicious_users.add(username)
+
+    return suspicious_ips, suspicious_users
 
 if __name__ == '__main__':
-    suspicious_login(logs)
+    logs = [
+    "2024-11-01T12:00:00,user1,192.168.1.1,FAILED",
+    "2024-11-01T12:03:00,user1,192.168.1.1,FAILED",
+    "2024-11-01T12:06:00,user1,192.168.1.1,FAILED",
+    "2024-11-01T12:08:00,user1,192.168.1.1,FAILED",
+    "2024-11-01T12:09:00,user1,192.168.1.2,SUCCESS",
+    "2024-11-01T12:12:00,user1,192.168.1.3,SUCCESS",
+    "2024-11-01T12:15:00,user2,192.168.1.4,FAILED"
+]
+    res = suspicious_login_detection(logs)
+    print(res)
     
     
         
